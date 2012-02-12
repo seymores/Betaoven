e01c7cb1c149a601580ed06769ced1ab0d962e6c
Tan Shiaw Uen <shiawuen@gmail.com>
2012-02-12 14:51:13 +0800

    Default data for project and invite

--- test/default-data.js
+++ test/default-data.js
@@ -1,11 +1,30 @@
 const User = require('../models/user')
+    , Project = require('../models/project')
+    , Invite = require('../models/invite')
     ;
 
-var user 
-    = exports.user 
+var user
+    = exports.user
     = { email: 'hello@betaoven.com', display_name: 'oven'}
 
   , feedback
     = exports.feedback
     = { by: new User(user), details: 'meow~' }
+
+  , project
+    = exports.project
+    = {
+        name: 'betaoven'
+      , owner: new User(user)
+      , description: 'Oven to cook android app'
+    }
+
+  , invite
+    = exports.invite
+    = {
+        email: 'invite@betaoven.com'
+      , project: new Project(project)
+      , from: new User(user)
+      , message: 'Welcome to betaoven!'
+    }
   ;
