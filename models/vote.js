6ee9ee5986bc4a8893a9ab1b278cfb2abdf9f1c0
Tan Shiaw Uen <shiawuen@gmail.com>
2012-02-12 16:22:46 +0800

    Vote model and tests

---
+++ models/vote.js
@@ -0,0 +1,30 @@
+const db = require('../db')
+    , Schema = require('mongoose').Schema
+    , ObjectId = Schema.ObjectId
+    ;
+
+
+var Vote = new Schema({
+    by: {
+        type: ObjectId
+      , ref: 'User'
+      , required: true
+    }
+  , build: {
+        type: ObjectId
+      , ref: 'Build'
+      , required: true
+    }
+  , point: {
+        type: Number
+      , required: true
+      , default: 0
+      , min: -1
+      , max: 1
+    }
+});
+
+
+var Model
+  = module.exports
+  = db.model('Vote', Vote);
---
+++ test/vote.model.test.js
@@ -0,0 +1,79 @@
+const Vote = require('../models/vote')
+    , should = require('should')
+    , getData = require('./default-data').vote
+    ;
+
+describe('Vote Model', function(){
+  describe('#save()', function(){
+    var vote, data, sets;
+
+    it('should save without error', function(done){
+      vote = new Vote(getData())
+
+      vote.save(function(err){
+        should.not.exist(err)
+
+        done()
+      })
+    })
+
+    sets = ['build', 'by'];
+    sets.forEach(function(path){
+      it('should prompt error when "' +path+ '" not set', function(done){
+        data = getData()
+
+        delete data[path]
+        vote = new Vote(data)
+
+        vote.save(function(err){
+          should.exist(err)
+
+          err.should.have.property('errors')
+          err.errors.should.have.property(path)
+
+          done()
+        })
+      })
+    })
+
+    it('should be default "point" to 0', function(done){
+      var point;
+
+      vote = new Vote(getData())
+      point = vote.point+''
+      point.should.eql('0')
+
+      done()
+    })
+
+    sets = [-1, 0, 1]
+    sets.forEach(function(v){
+      it('should be ok if "point" within -1 to 1, point = '+v, function(done){
+        vote = new Vote(getData())
+        vote.point = v
+
+        vote.save(function(err){
+          should.not.exist(err)
+          done()
+        })
+      })
+    })
+
+    sets = [-10, 100, 2]
+    sets.forEach(function(v){
+      it('should not be ok if "point" not within -1 to 1, point = '+v, function(done){
+        vote = new Vote(getData())
+        vote.point = v
+
+        vote.save(function(err){
+          should.exist(err)
+
+          err.errors.should.have.property('point')
+          err.errors.point.type.should.be.eql(v > 0 ? 'max' : 'min')
+
+          done()
+        })
+      })
+    })
+  })
+})
