d4676973498cd8757eb18475e8b015bf47faed91
Tan Shiaw Uen <shiawuen@gmail.com>
2012-02-12 16:23:24 +0800

    Expose all models in one go

---
+++ models/index.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    Vote: require('./vote')
+  , Build: require('./build')
+  , User: require('./user')
+  , Invite: require('./invite')
+  , Project: require('./project')
+  , Feedback: require('./feedback')
+}
