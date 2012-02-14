966620c40c246abe262f6cff0fd9ffe50aa95dcd
Tan Shiaw Uen <shiawuen@gmail.com>
2012-02-14 13:27:40 +0800

    Better logic for detection

--- middlewares/mobile-detector.js
+++ middlewares/mobile-detector.js
@@ -1,10 +1,27 @@
 
+/**
+ * Mobile Detector
+ *
+ * Logic based on solution from http://notnotmobile.appspot.com/
+ */
+
 module.exports = function(options){
-  var pattern = /Mobile Safari|Fennec|Opera Mobi/;
+  var mobile = /iphone|ipod|blackberry|android|palm|windows ce/gi
+    , desktop = /windows|linux|os x|solaris|bsd/gi
+    , bot = /spider|crawl|slurp|bot/gi;
 
   return function(req, res, next){
-    req.isMobile = pattern.test(req.headers['user-agent'])
+    var ua = req.headers['user-agent'];
 
-    next()
+    /**
+     * Anything that looks like a phone isn't a desktop.
+     * Anything that looks like a desktop probably is.
+     * Anything that looks like a bot should default to desktop.
+     */
+
+    req.isMobile = mobile.test(ua) && !(desktop.test(ua) || bot.test(ua));
+    req.isDesktop = !req.isMobile;
+
+    next();
   }
 }
