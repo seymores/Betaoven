5c1bf00973ad38f3e553e482f7b98ded0f99e7e5
Tan Shiaw Uen <shiawuen@gmail.com>
2012-02-17 20:42:56 +0800

    whitespace

--- app.js
+++ app.js
@@ -18,22 +18,22 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  
+
   app.use(express.cookieParser());
   app.use(express.session({ secret: config.session.secret }))
-  
+
   app.use(middlewares.mobileDetector());
-  
+
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+  app.use(express.errorHandler());
 });
 
 // Routes
