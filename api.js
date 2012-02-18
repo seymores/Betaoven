4a95f73556c3cee98c1d632556fcae213d5728f1
Tan Shiaw Uen <shiawuen@gmail.com>
2012-02-18 17:38:56 +0800

    Expose feedbacks API

--- api.js
+++ api.js
@@ -10,6 +10,7 @@ const restify = require('restify')
 const users = require('./routes/api/users')
     , projects = require('./routes/api/projects')
     , builds = require('./routes/api/builds')
+    , feedbacks = require('./routes/api/feedbacks')
     ;
 
 
@@ -32,6 +33,9 @@ api.get('/project/:pid/invite', projects.invite)
 api.get('/project/:pid/builds', builds.list)
 api.post('/project/:pid/builds', builds.create)
 api.get('/project/:pid/build/:bid', builds.get)
+
+api.get('/project/:pid/build/:bid/feedbacks', feedbacks.list)
+api.post('/project/:pid/build/:bid/feedbacks', feedbacks.create)
 
 !module.parent
   && api.listen(config.site.port)
