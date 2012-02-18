4cd85645300a310a1ae3d3c4dc6ffc7f93168c09
Tan Shiaw Uen <shiawuen@gmail.com>
2012-02-18 20:18:28 +0800

    new sample data for init

--- scripts/populate-test-data.js
+++ scripts/populate-test-data.js
@@ -1,34 +1,89 @@
 const models = require('../models')
     , User = models.User
+    , Project = models.Project
+    , Build = models.Build
     , db = require('../db')
     ;
 
 var users = []
-  , sets = 20
-  , n = sets
-  , completed = 0
-  , name
-  ;
+  , projects = []
+  , builds = [];
 
-users[0] = new User({
-    email: 'shiawuen@gmail.com'
-  , display_name: 'shiawuen'
-  , password: 'b@ta0ven'
-})
 
-for (;n;){
-  name = 'user'+(n--)
-  users.push(new User({
-      email: name+'@betaoven.com'
-    , display_name: name
+addUsers();
+
+
+function addUsers(){
+  var sets = 20
+    , n = sets
+    , completed = 0
+    , name
+    ;
+
+  users[0] = new User({
+      email: 'shiawuen@gmail.com'
+    , username: 'shiawuen'
+    , display_name: 'shiawuen'
     , password: 'b@ta0ven'
-  }))
-}
+  })
 
-User.remove(function(){
-  users.forEach(function(user){
-    user.save(function(){
-      ++completed === sets && db.disconnect()
+  users[1] = new User({
+      email: 'anonymous@betaoven.com'
+    , username: 'anonymous'
+    , display_name: 'anonymous'
+    , password: 'b@ta0ven'
+  });
+
+  sets += 2;
+
+  for (;n;){
+    name = 'user'+(n--)
+    users.push(new User({
+        email: name+'@betaoven.com'
+      , username: name
+      , display_name: name
+      , password: 'b@ta0ven'
+    }))
+  }
+
+  User.remove(function(){
+    console.log('Removed all users')
+    users.forEach(function(user){
+      user.save(function(){
+        ++completed === sets && addProjects()
+      })
     })
   })
-})
+}
+
+function addProjects(){
+  var completed = 0
+    , sets = 0
+    ;
+
+  projects[0] = new Project({
+      name: 'Betaoven'
+    , description: 'The official baker'
+    , admins: [users[1]]
+    , collaborators: [users[1]]
+  });
+
+  sets += 1;
+
+  Project.remove(function(){
+    console.log('Removed all projects')
+    projects.forEach(function(project){
+      project.save(function(err){
+        ++completed === sets && addBuilds()
+      })
+    })
+  })
+}
+
+
+function addBuilds(){
+  Build.remove(function(){
+    console.log('Removed all builds')
+    db.disconnect();
+  })
+}
